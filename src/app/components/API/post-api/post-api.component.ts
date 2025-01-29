21ba993316d26286defd5540a4296ed6733fb52c
Josimar Reyes Campos <darth_jos@hotmail.com>
2025-01-29 12:38:55 -0600

    Package-lock updates and add PUT & DELETE API examples

--- src/app/components/API/post-api/post-api.component.ts
+++ src/app/components/API/post-api/post-api.component.ts
@@ -22,14 +22,16 @@ export class PostApiComponent {
     "regNo":""
   }
 
-  http = inject(HttpClient);
+  http = inject(HttpClient);  //<-- inyecta el módulo de HttpClient para poder usarse en este componente
 
+  //EJEMPLO de consumir GET API --> consulta un registro
   getAllCars() {
     this.http.get("https://freeapi.miniprojectideas.com/api/CarRentalApp/GetCars").subscribe((res:any) => {
       this.carList = res.data;
     })
   }
 
+  //EJEMPLO uso de api con POST -->  Crea un registro
   onSaveCar() {
     this.http.post("https://freeapi.miniprojectideas.com/api/CarRentalApp/CreateNewCar", this.carObj).subscribe((res:any) => {
       if(res.result) {
@@ -40,5 +42,40 @@ export class PostApiComponent {
         alert(res.message)
       }
     })
+  }
+
+  //EJEMPLO uso de API con PUT --> Actualiza un registro
+  updateCar() {
+    this.http.put("https://freeapi.miniprojectideas.com/api/CarRentalApp/UpdateCar", this.carObj).subscribe((res:any) => {
+      if(res.result) {
+        alert("Car Updated Successfully");
+        this.getAllCars();
+      } else {
+        alert(res.message);
+      }
+    })
+  }
+
+  //Esta función solo es para cargar los datos del objeto de carro para presentar en el formulario
+  onEdit(data: any) {
+    this.carObj = data;
+  }
+
+  //EJEMPLO uso de API con DELETE --> Elimina un registro
+  onDelete(id: number) {
+
+    const isDelete = confirm("Are you sure you want to delete? ")
+
+    if(isDelete == true) {
+      this.http.delete("https://freeapi.miniprojectideas.com/api/CarRentalApp/DeleteCarbyId?carid=" + id).subscribe((res:any)=>{
+        if(res.result) {
+          alert("Car Deleted Successfully");
+          this.getAllCars();
+        } else {
+          alert(res.message);
+        }
+      })
+    }
+
   }
 }
